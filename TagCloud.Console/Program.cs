babec8f22a859ba436c8f3923d90ad4771e0f76b
stepan <stepan.zakolyukin@yandex.ru>
2024-12-17 14:06:26 +0500

    Перенес свои наработки из репозитория tdd

--- TagCloud.Console/Program.cs
+++ TagCloud.Console/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Drawing;
 
-Console.WriteLine("Hello, World!");
+namespace TagCloud.Console;
+
+internal class Program
+{
+    private const string Path = "../../../Images";
+
+    static void Main()
+    {
+        var colors = new[] { Color.Red, Color.Green, Color.Brown, Color.Yellow, Color.Blue };
+
+        var visual = new VisualizationCloudLayout(800, 600, Color.White, colors);
+
+        visual.CreateImage(new CircularCloudLayouter(new Point(400, 300)), 175, new Size(30, 5), new Size(100, 25))
+            .Save($"{Path}/CentralСloud.png");
+
+        visual.CreateImage(new CircularCloudLayouter(new Point(250, 150)), 50, new Size(30, 5), new Size(80, 25))
+            .Save($"{Path}/SmalСloud.png");
+    }
+}
---
+++ TagCloud/VisualizationCloudLayout.cs
@@ -0,0 +1,58 @@
+﻿using System.Drawing;
+
+namespace TagCloud;
+
+public class VisualizationCloudLayout
+{
+    private readonly int width;
+    private readonly int height;
+    private readonly Color backgroundColor;
+    private readonly Color[] rectanglePalette;
+
+    public VisualizationCloudLayout(int width, int height, Color backgroundColor, IEnumerable<Color> rectanglePalette)
+    {
+        this.width = width;
+        this.height = height;
+        this.backgroundColor = backgroundColor;
+        this.rectanglePalette = rectanglePalette.ToArray();
+    }
+
+    public Bitmap CreateImage(CircularCloudLayouter cloudLayouter, int amountRectangles,
+        Size minSize, Size maxSize)
+    {
+        var image = new Bitmap(width, height);
+        var rectangles = CloudLayout.GenerateCloudLayout(
+            amountRectangles,
+            minSize,
+            maxSize,
+            cloudLayouter);
+
+        DrawCloudLayout(Graphics.FromImage(image), rectangles);
+
+        return image;
+    }
+
+    public Bitmap CreateImage(IEnumerable<Rectangle> rectangles)
+    {
+        var image = new Bitmap(width, height);
+
+        DrawCloudLayout(Graphics.FromImage(image), rectangles);
+
+        return image;
+    }
+
+    private void DrawCloudLayout(Graphics graphics, IEnumerable<Rectangle> rectangles)
+    {
+        var random = new Random();
+
+        graphics.FillRectangle(new SolidBrush(backgroundColor), 0, 0, width, height);
+
+        foreach (var rect in rectangles)
+        {
+            var color = rectanglePalette[random.Next(rectanglePalette.Length)];
+
+            graphics.FillRectangle(new SolidBrush(color), rect);
+            graphics.DrawRectangle(new Pen(Color.Black, 1), rect);
+        }
+    }
+}
